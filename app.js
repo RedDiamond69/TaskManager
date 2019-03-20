253e22ef4934d0f413695668990a957a99d23cf2
Alexey Kosyuk <reddiamond821@gmail.com>
2019-03-21 02:00:01 +0300

    Change view engine to pug.

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
 const express = require('express');
 const path = require('path');
 const console = require('console');
+const logger = require('morgan');
 const app = express();
 const indexRouter = require('./routes/index');
 
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'pug');
 
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({
-    extended: false
+    extended: true
 }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 
+app.use((req, res, next) => {
+    let err = new Error('Page not found.');
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.render('error.pug', {
+        status: err.status,
+        message: err.message
+    });
+    next();
+});
+
 app.listen(3000, () => {
-    console.log('Server start listening at port 3000.');
+    console.log('Server start listening on port number 3000.');
 });
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (require, response) => {
-    response.render('index', {
+router.get('/', (req, res) => {
+    res.render('index.pug', {
         title: 'Index Page'
     });
 });
