d5506cfecb9193375479d928b24c67bb34187102
Alexey Kosyuk <reddiamond821@gmail.com>
2019-03-24 03:07:40 +0300

    Add layout page, images, partials. Connect bootstrap.

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index.pug', {
-        title: 'Index Page'
+    res.render('index', {
+        title: 'Task Manager'
     });
+});
+
+router.get('/add', (req, res) => {
+
+});
+
+router.post('/add', (req, res) => {
+
 });
 
 module.exports = router;
